d0cedc3ab6cbfcea924e250dc0fc10e95a1a10d1
Sattar Hummatli <settarxan@gmail.com>
2017-11-29 19:38:29 +0400

    add two way binding

--- angular-framework-intro/src/app/app.module.ts
+++ angular-framework-intro/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+ 
 
 import { AppComponent } from './app.component';
 import { UsernameComponent } from './username/username.component';
@@ -12,7 +13,8 @@ import { UsernameComponent } from './username/username.component';
     UsernameComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
